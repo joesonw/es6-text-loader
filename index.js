2a7a92d5bf62a8ecbaa0afa91faef8bdc0f2522f
Qiaosen Huang <joesonw@gmail.com>
2016-12-12 12:00:39 +0800

    fix

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ module.exports = function (content) {
     this.cacheable && this.cacheable();
     this.value = content;
     var result = '';
-    result += `Object.defineProperty(exports, "__esModule", { value: true });\n';
+    result += 'Object.defineProperty(exports, "__esModule", { value: true });\n';
     result += 'exports.default = '  + JSON.stringify(content);
     return result;
 }
